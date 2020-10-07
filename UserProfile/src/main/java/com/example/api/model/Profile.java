de2b436df9512a5c2b9828c07e9e2b70dd9a96d0
push <push@DESKTOP-HHU30EU>
2020-10-07 17:11:23 +0530

    updated with date value in user profile

--- UserProfile/src/main/java/com/example/api/model/Profile.java
+++ UserProfile/src/main/java/com/example/api/model/Profile.java
@@ -1,9 +1,11 @@
 package com.example.api.model;
 
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,16 +21,21 @@ public class Profile {
 	private int age;
 	private long mobileNumber;
 	
-	public Profile() {
-		
-	}
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	private Date createdDate = new Date();
 
-	public Profile(ObjectId _id, String firstName, String lastName, int age, long mobileNumber) {
+	public Profile() {
+	
+	}
+	
+	public Profile(ObjectId _id, String firstName, String lastName, int age, long mobileNumber, Date createdDate) {
+		super();
 		this._id = _id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
 		this.mobileNumber = mobileNumber;
+		this.createdDate = createdDate;
 	}
 
 	public String get_id() {
@@ -63,10 +70,20 @@ public class Profile {
 		this.mobileNumber = mobileNumber;
 	}
 	
+	public Date getCreatedDate() {
+	    return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+	    this.createdDate = createdDate;
+	}
+
 	@Override
 	public String toString() {
 		return "Profile [_id=" + _id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
-				+ ", mobileNumber=" + mobileNumber + "]";
+				+ ", mobileNumber=" + mobileNumber + ", createdDate=" + createdDate + "]";
 	}
+	
+	
 	
 }
